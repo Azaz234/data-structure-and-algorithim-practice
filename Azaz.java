df460f9e59cc676b43ba6b3158be03867411b11f
Mohd Azaz Khatri <62958233+Azaz234@users.noreply.github.com>
2020-04-03 19:03:43 +0530

    star pattern in rectangle

--- Azaz.java
+++ Azaz.java
@@ -1,7 +1,14 @@
-public class Azaz
+class Azaz
 {
 public static void main(String args[])
 {
-System.out.println("hello");
+  for(int i=1;i<=5;i++)
+  {
+  for(int j=1;j<=5;j++)
+  {
+  System.out.print("*");
+  }
+  System.out.println();
+  }
 }
 }
