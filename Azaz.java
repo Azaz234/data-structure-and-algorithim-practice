547acea31cc6b36d9e96d3698e416448dd0f2baa
Mohd Azaz Khatri <62958233+Azaz234@users.noreply.github.com>
2020-04-05 16:18:26 +0530

    number System

--- Azaz.java
+++ Azaz.java
@@ -1,23 +1,16 @@
 class Azaz
 {
-public static void main(String args[])
+public static void main (String args[])
+{ 
+int count=0;
+for(int i=1;i<=5;i++)
 {
-  for(int i=1;i<=5;i++)
-  {
-  for(int j=1;j<=5;j++)
-  {
-	  if(i>=2&&j>=2&&i<=4&&j<=4)
-  {
-	System.out.print(" "); 
-  }
-  else
-  {
-  System.out.print("*");
-  }
-  }
-  System.out.println();
-  }
+for(int j=1;j<=3;j++)
+{
+	count=count+1;
+System.out.print(count+"");
+}
+System.out.println();
 }
 }
-
-
+}
