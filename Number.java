af074e2d47fd8f1a0df7c93f135f5e3ce55861f0
Mohd Azaz Khatri <62958233+Azaz234@users.noreply.github.com>
2020-04-07 19:48:13 +0530

    number trianle

---
+++ Number.java
@@ -0,0 +1,19 @@
+public class Number
+{
+public static void main(String args[])
+{
+  int k=1,p=1;
+  for(int i=1;i<=4;i++)
+  {
+	  for(int s=4;s>i;s--)
+		  System.out.print(" ");
+  for(int j=1;j<=p;j++)
+  {
+  System.out.print(k);
+  k=k+1;
+  }
+  p=p+2;
+  System.out.println();
+  }
+  }
+  }
